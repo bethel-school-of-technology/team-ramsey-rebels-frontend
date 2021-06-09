b486a2eb37b05ad0ced942db3268273b05c3daa4
BenC <fpubenc@gmail.com>
2021-06-09 16:06:03 -0500

    added landing component boilerplate

--- mechanicnow-frontend/src/App.js
+++ mechanicnow-frontend/src/App.js
@@ -1,13 +1,17 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Signup from './Signup';
+
+import Landing from './components/Landing/Landing';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>MechanicNow</h1>
-        <Signup />
+
+        <Landing />
       </header>
     </div>
   );
---
+++ mechanicnow-frontend/src/components/Landing/Landing.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+const Landing = () => (
+  <div>
+    Landing Component
+  </div>
+);
+
+Landing.propTypes = {};
+
+Landing.defaultProps = {};
+
+export default Landing;
