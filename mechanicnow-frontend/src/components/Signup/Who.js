8ee017b26b3ca49384f64e5d001005e4bccfa806
Dalton Zebrak <dzebrak12@gmail.com>
2021-06-10 00:17:12 -0500

    created Signup folder for signup component and added 3 component files for Who page, Car Owner Nav, and Car Owner Request

--- mechanicnow-frontend/src/components/Signup/Who.js
+++ mechanicnow-frontend/src/components/Signup/Who.js
@@ -4,6 +4,7 @@ function Who() {
     return(
         <div>
             <h2>Who Are You?</h2>
+            <hr></hr>
             <button>Car Owner</button>
             <button>Mechanic</button>
         </div>
