f7c14570410fca02861831445cce4dae1068cb2b
BenC <fpubenc@gmail.com>
2021-06-10 11:58:31 -0500

    added routing to App

--- mechanicnow-frontend/src/App.js
+++ mechanicnow-frontend/src/App.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 import Landing from './components/Landing/Landing';
+import OwnerReq from './components/OwnerReq';
+import OwnerNav from './components/OwnerNav';
+import Who from './components/Signup/Who'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>MechanicNow</h1>
-        <Landing />
-      </header>
+    <Router>
+    <div>
+      <Route exact path="/" component={Landing} />
+      <Route path="/who" component={Who} />
+      <Route path="/OwnerReq" component={OwnerReq} />
+      <Route path="/OwnerNav" component={OwnerNav} />
     </div>
+    </Router>
   );
 }
+
+
 
 export default App;
--- mechanicnow-frontend/src/components/Landing/Landing.js
+++ mechanicnow-frontend/src/components/Landing/Landing.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
   function tempAlert(){
     alert("You just clicked to sign in.");
