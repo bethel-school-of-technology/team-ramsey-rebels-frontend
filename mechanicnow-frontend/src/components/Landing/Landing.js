727222a5dbebceeb4529be95cf87ba58231dd39c
Ben Cunningham <fpubenc@gmail.com>
2021-06-09 23:08:57 -0500

    added sign in button

--- mechanicnow-frontend/src/components/Landing/Landing.js
+++ mechanicnow-frontend/src/components/Landing/Landing.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+  function tempAlert(){
+    alert("You just clicked to sign in.");
+  };
 
 const Landing = () => (
+
   <div>
-    Landing Component
+    <div>
+    Welcome to MechanicNow
+    </div>
+    <button onClick={tempAlert}>
+      Sign In with Google
+    </button>
   </div>
 );
 
