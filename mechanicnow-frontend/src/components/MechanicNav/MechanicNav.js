7afbb90388429b34f4e3571c2bfa1ed7ad94d43d
Bmattholder <matt.holder@ramseysolutions.com>
2021-06-11 13:31:59 -0500

    made mechanic nav barebones

---
+++ mechanicnow-frontend/src/components/MechanicNav/MechanicNav.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+const MechanicNav = () => (
+  <div>
+    MechanicNav Component
+    <button>Find new jobs!</button>
+  </div>
+  
+);
+
+MechanicNav.propTypes = {};
+
+MechanicNav.defaultProps = {};
+
+export default MechanicNav;
