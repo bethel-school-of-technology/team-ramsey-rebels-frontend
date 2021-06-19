3a46cb8242ec988cc07d7d845ca70663d84d1684
fpubenc <73669447+fpubenc@users.noreply.github.com>
2021-06-18 23:00:39 -0500

    added bootstrap styling for landing and about components (#17)

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,18 +2,40 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Navigation from './Nav/Navigation';
+import { Container, Row, Col, Image } from 'react-bootstrap';
 
 const Landing = () => (
 
   <div>
     <Navigation />
+    <Container>
+      <Row>
+        <Col>
     <h1>
     Welcome to MechanicNow!
     </h1>
-    <h3>
+    </Col>
+    </Row>
+    <Row>
+    <Col>
+    <Image src="https://images.unsplash.com/photo-1494976388531-d1058494cdd8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Nnx8fGVufDB8fHx8&w=1000&q=80" roundedCircle fluid alt="nicecar"/>
+    </Col>
+      <Col>
+    <p>
       Find your way to mechanical nirvana with our industry-standard tire changes and lubrication services.
-    </h3>
-    <img src="https://images.unsplash.com/photo-1494976388531-d1058494cdd8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Nnx8fGVufDB8fHx8&w=1000&q=80" alt="nicecar"/>
+    </p>
+    </Col>
+    </Row>
+    <br />
+    <Row>
+      <Col>
+      <p>There is no one happier to come work on your car than our trustworthy mechanics. They do whatever you ask them to do. Anything.</p>
+      </Col>
+      <Col>
+      <Image src="https://cdn.crello.com/api/media/small/392799810/stock-photo-selective-focus-happy-mechanic-overalls" roundedCircle fluid />
+      </Col>
+    </Row>
+    </Container>
   </div>
 );
 
