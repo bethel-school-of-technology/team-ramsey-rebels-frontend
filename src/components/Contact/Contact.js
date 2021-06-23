239b1ed9f5279323bd1d8bd02b286dbd3b6d1ffc
Bmattholder <matt.holder@ramseysolutions.com>
2021-06-22 21:50:58 -0500

    added admin pages

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -7,7 +7,12 @@ const Contact = () => {
         <div>
             <Navigation />
             <br />
-            <h2 id="nav">Contact Us</h2>     
+            <h2 id="nav">Contact Us</h2>  
+            <br />
+            <br />
+            <p>Rain, snow or shine, we'll be sure that your car is fine.</p>   
+            <p>Please send all complaints to Ben Cunningham: 615.645.6015</p>
+            <p>Hours of operation: Tuesdays 9-10 am</p>
         </div>
     )
 }
