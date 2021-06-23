bab1bc77210d5186e8adc876dd030c4e1313db8d
Dalton Zebrak <dzebrak12@gmail.com>
2021-06-22 21:56:29 -0500

    styled landing page to mirror wireframe

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,6 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
 import Navigation from "./Nav/Navigation";
 import { Container, Row, Col, Image } from "react-bootstrap";
 
@@ -8,42 +6,7 @@ const Landing = () => (
   <div>
     <Navigation />
     <Container>
-      <br />
-      <Row>
-        <Col>
-          <Image
-            src="https://images.unsplash.com/photo-1494976388531-d1058494cdd8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Nnx8fGVufDB8fHx8&w=1000&q=80"
-            alt="nicecar"
-            roundedCircle
-            fluid
-          />
-        </Col>
-        <Col>
-        <br />
-          <p>
-            Find your way to mechanical nirvana with our industry-standard tire
-            changes and lubrication services.
-          </p>
-        </Col>
-      </Row>
-      <br />
-      <Row>
-        <Col sm={6}>
-        <br />
-          <p>
-            There is no one happier to come work on your car than our
-            trustworthy mechanics. They do whatever you ask them to do.
-            Anything.
-          </p>
-        </Col>
-        <Col sm={6}>
-          <Image
-            src="https://cdn.crello.com/api/media/small/392799810/stock-photo-selective-focus-happy-mechanic-overalls"
-            roundedCircle
-            fluid
-          />
-        </Col>
-      </Row>
+      
     </Container>
   </div>
 );
