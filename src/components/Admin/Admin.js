b90537bfc09397d6f37db909d526b2f5811b4f72
Dalton Zebrak <dzebrak12@gmail.com>
2021-06-23 02:17:21 -0500

    added styling/organized admin component

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -3,6 +3,7 @@ import firebase from "firebase";
 import AdminInput from "./AdminInput";
 import Navigation from "../Nav/Navigation";
 import Button from "react-bootstrap/Button";
+import { Card, Container } from "react-bootstrap";
 
 function Admin() {
   const [requests, setRequests] = React.useState([]);
@@ -25,23 +26,34 @@ function Admin() {
       <Navigation />
       <br />
       <br />
-      <h2>Update service requests here:</h2>
-      <div className="refresh">
-        <Button variant="success" onClick={refreshPage}>
-          Refresh Changes
-        </Button>
-      </div>
-      <hr />
-      <div className="requests">
-        {requests.map((request) => (
-          <div key={request.serviceType}>
-            <AdminInput request={request} />
-          </div>
-        ))}
-        <Button variant="success" onClick={refreshPage}>
-          Refresh Changes
-        </Button>
-      </div>
+      <Container>
+        <Card>
+          <Card.Header>
+            <h2>Update Service Requests Here:</h2>
+          </Card.Header>
+          <Card.Body>
+            <Card.Title></Card.Title>
+            <Card.Text>
+              <div className="refresh">
+                <Button variant="success" onClick={refreshPage}>
+                  Refresh Changes
+                </Button>
+              </div>
+              <hr />
+              <div className="requests">
+                {requests.map((request) => (
+                  <div key={request.serviceType}>
+                    <AdminInput request={request} />
+                  </div>
+                ))}
+                <Button variant="success" onClick={refreshPage}>
+                  Refresh Changes
+                </Button>
+              </div>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Container>
     </div>
   );
 }
--- src/components/Admin/AdminInput.js
+++ src/components/Admin/AdminInput.js
@@ -1,6 +1,7 @@
 import React from "react";
 import firebase from "firebase";
 import Button from "react-bootstrap/Button";
+import { Container, Card } from "react-bootstrap";
 
 const AdminInput = ({ request }) => {
   const [serviceType, setServiceType] = React.useState(request.serviceType);
@@ -19,31 +20,43 @@ const AdminInput = ({ request }) => {
 
   return (
     <>
-      <p>
-        <b>Request ID: </b>
-        {request.id}
-      </p>
-      <p>
-        {request.fullName} | {request.email} | {request.phone}
-      </p>
-      <p>
-        {request.vehicle} | {request.location}
-      </p>
-      Service Needed:{" "}
-      <input
-        value={serviceType}
-        onChange={(e) => {
-          setServiceType(e.target.value);
-        }}
-      />
       <br />
-      <Button variant="warning" onClick={onUpdate}>
-        Update
-      </Button>
-      <Button variant="danger" onClick={onDelete}>
-        Delete
-      </Button>
-      <hr />
+      <Container>
+        <Card>
+          <Card.Header>
+            <b>Request ID: </b>
+            {request.id}
+          </Card.Header>
+          <Card.Body>
+            <Card.Title>
+              <p>
+                {request.fullName} | {request.email} | {request.phone}
+              </p>
+            </Card.Title>
+            <Card.Text>
+              <p>
+                {request.vehicle} | {request.location}
+              </p>
+              Service Needed:{" "}
+              <input
+                value={serviceType}
+                onChange={(e) => {
+                  setServiceType(e.target.value);
+                }}
+              />
+              <Button variant="warning" onClick={onUpdate}>
+                Update
+              </Button>
+              <br />
+              <br />
+              <Button variant="danger" onClick={onDelete}>
+                Delete
+              </Button>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Container>
+      <br />
     </>
   );
 };
