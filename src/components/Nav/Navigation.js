553a138ddfe525ff9286ed8aceaf30c092383158
Dalton Zebrak <dzebrak12@gmail.com>
2021-06-22 21:11:33 -0500

    enhanced navbar responsiveness

--- src/components/Nav/Navigation.js
+++ src/components/Nav/Navigation.js
@@ -1,17 +1,21 @@
 import React from "react";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
+import { Nav, Navbar, Container } from "react-bootstrap";
 
 const Navigation = () => (
   <div>
-    <Navbar bg="light" variant="light">
-      <Navbar.Brand variant="primary">MechanicNow</Navbar.Brand>
-      <Nav variant="pills" className="mr-auto">
-        <Nav.Link href="/landing">Home</Nav.Link>
-        <Nav.Link href="/newrequest">New Service</Nav.Link>
-        <Nav.Link href="/about">About</Nav.Link>
-        <Nav.Link href="/contactus">Contact Us</Nav.Link>
-      </Nav>
+    <Navbar collapseOnSelect expand="sm" bg="light" variant="light">
+      <Container>
+        <Navbar.Brand variant="primary">MechanicNow</Navbar.Brand>
+        <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+        <Navbar.Collapse id='responsive-navbar-nav'>
+        <Nav>
+          <Nav.Link href="/landing">Home</Nav.Link>
+          <Nav.Link href="/newrequest">New Service</Nav.Link>
+          <Nav.Link href="/about">About</Nav.Link>
+          <Nav.Link href="/contactus">Contact Us</Nav.Link>
+        </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   </div>
 );
