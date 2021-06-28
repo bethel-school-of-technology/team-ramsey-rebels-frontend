374c1fb7c90f95d84fe6ff449384926b46ede609
Dalton Zebrak <dzebrak12@gmail.com>
2021-06-27 23:02:28 -0500

    practice with carousel customization

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -7,42 +7,80 @@ const Landing = () => (
     <Navigation />
     <br />
     <br />
-    <Carousel>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://s.abcnews.com/images/Business/mustang3-ht-ml-190114_hpEmbed_3x2_992.jpg"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>Welcome to MechanicNow!</h3>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <Carousel.Caption>
-          <h3>WE work while YOU work so your car is ready to go!</h3> 
-        </Carousel.Caption>
-        <img
-          className="d-block w-100"
-          src="https://cars.usnews.com/pics/size/640x420/images/article/202008/127958/15_DSC03872_2_640x420.jpg"
-          alt="Second slide"
-        />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://www.cnet.com/a/img/Lq0pr9izf6rW5zlt5BrCQ9Lq40g=/940x0/2019/10/25/9c965540-3447-48a8-b1e4-3a7caf42b58e/2020-ford-mustang-shelby-gt500-08.jpg"
-          alt="Third slide"
-        />
+    <Container>
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://s.abcnews.com/images/Business/mustang3-ht-ml-190114_hpEmbed_3x2_992.jpg"
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>Welcome to MechanicNow!</h3>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <Carousel.Caption>
+            <h3>WE work while YOU work so your car is ready to go!</h3>
+          </Carousel.Caption>
+          <img
+            className="d-block w-100"
+            src="https://d3lp4xedbqa8a5.cloudfront.net/s3/digital-cougar-assets/whichcar/2019/01/15/65642/2020-Ford-Mustang-Shelby-GT500-revealed-news-(1).jpg"
+            alt="Second slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://www.cnet.com/a/img/Lq0pr9izf6rW5zlt5BrCQ9Lq40g=/940x0/2019/10/25/9c965540-3447-48a8-b1e4-3a7caf42b58e/2020-ford-mustang-shelby-gt500-08.jpg"
+            alt="Third slide"
+          />
 
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </Container>
+    <br />
+    <br />
+    <Container>
+      <Carousel>
+        <Carousel.Item interval={500}>
+          <img
+            className="d-block w-100"
+            src="https://s.abcnews.com/images/Business/mustang3-ht-ml-190114_hpEmbed_3x2_992.jpg"
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>Welcome to MechanicNow!</h3>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={500}>
+          <Carousel.Caption>
+            <h3>WE work while YOU work so your car is ready to go!</h3>
+          </Carousel.Caption>
+          <img
+            className="d-block w-100"
+            src="https://d3lp4xedbqa8a5.cloudfront.net/s3/digital-cougar-assets/whichcar/2019/01/15/65642/2020-Ford-Mustang-Shelby-GT500-revealed-news-(1).jpg"
+            alt="Second slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item interval={500}>
+          <img
+            className="d-block w-100"
+            src="https://www.cnet.com/a/img/Lq0pr9izf6rW5zlt5BrCQ9Lq40g=/940x0/2019/10/25/9c965540-3447-48a8-b1e4-3a7caf42b58e/2020-ford-mustang-shelby-gt500-08.jpg"
+            alt="Third slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </Container>
+    <br />
+    <br />
   </div>
 );
 
