ce31009b5b38df80d461e4ae4ee0a87dd0d1ca83
Dalton Zebrak <dzebrak12@gmail.com>
2021-06-29 11:58:55 -0500

    tweaked contact page

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -26,42 +26,34 @@ const Contact = () => {
         <Card>
           <Card.Img variant="top"/>
           <Card.Body>
-            <Card.Title>Card title</Card.Title>
+            <Card.Title>Ben Cunningham</Card.Title>
             <Card.Text>
-              This is a wider card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
             </Card.Text>
           </Card.Body>
           <Card.Footer>
-            <small className="text-muted">Last updated 3 mins ago</small>
+            <small className="text-muted">Director of Tire and Lube Services</small>
           </Card.Footer>
         </Card>
         <Card>
           <Card.Img variant="top"/>
           <Card.Body>
-            <Card.Title>Card title</Card.Title>
+            <Card.Title>Matt Holder</Card.Title>
             <Card.Text>
-              This card has supporting text below as a natural lead-in to
-              additional content.{" "}
             </Card.Text>
           </Card.Body>
           <Card.Footer>
-            <small className="text-muted">Last updated 3 mins ago</small>
+            <small className="text-muted">Director Marketing and Sales</small>
           </Card.Footer>
         </Card>
         <Card>
           <Card.Img variant="top" />
           <Card.Body>
-            <Card.Title>Card title</Card.Title>
+            <Card.Title>Dalton Zebrak</Card.Title>
             <Card.Text>
-              This is a wider card with supporting text below as a natural
-              lead-in to additional content. This card has even longer content
-              than the first to show that equal height action.
             </Card.Text>
           </Card.Body>
           <Card.Footer>
-            <small className="text-muted">Last updated 3 mins ago</small>
+            <small className="text-muted">Director of Customer Service</small>
           </Card.Footer>
         </Card>
       </CardGroup>
