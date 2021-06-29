1e81e5d3479357e1ab2075cb5eac65b282052ff8
Dalton Zebrak <dzebrak12@gmail.com>
2021-06-28 22:02:52 -0500

    added icons to navbar

--- src/components/Nav/Navigation.js
+++ src/components/Nav/Navigation.js
@@ -1,19 +1,20 @@
 import React from "react";
 import { Nav, Navbar, Container } from "react-bootstrap";
+import { VscTools, VscAccount, VscHome, VscAdd, VscRepo, VscMention } from "react-icons/vsc";
 
 const Navigation = () => (
   <div className="nav">
     <Navbar collapseOnSelect fixed="top" expand="md" bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand variant="primary">MechanicNow</Navbar.Brand>
+        <Navbar.Brand variant="primary"><VscTools /> MechanicNow</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav>
-            <Nav.Link href="/landing">Home</Nav.Link>
-            <Nav.Link href="/newrequest">New Service</Nav.Link>
+            <Nav.Link href="/landing">Home <VscHome /></Nav.Link>
+            <Nav.Link href="/newrequest">New Service <VscAdd /></Nav.Link>
             <Nav.Link href="/about">About</Nav.Link>
             <Nav.Link href="/contactus">Contact Us</Nav.Link>
-            <Nav.Link href="/adminlogin">Admin</Nav.Link>
+            <Nav.Link href="/adminlogin">Admin <VscAccount/></Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
